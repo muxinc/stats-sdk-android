d018ee09716a5283b892fa395a492ab1d880bea6
Emily Dixon <edixon@mux.com>
2022-10-13 14:56:04 -0700

    Test Connectivity Type

--- core-android/src/test/java/com/mux/core_android/AndroidDeviceTests.kt
+++ core-android/src/test/java/com/mux/core_android/AndroidDeviceTests.kt
@@ -16,72 +16,121 @@ import org.junit.Test
 class AndroidDeviceTests : AbsRobolectricTest() {
 
   @Test
-  fun testConnectionTypeIsCellular() {
+  fun testConnectivityCases() {
+    // Cellular Networks
+    testConnectivityCase(
+      connMgrReturns16 = listOf(ConnectivityManager.TYPE_MOBILE),
+      connMgrReturns23 = listOf(NetworkCapabilities.TRANSPORT_CELLULAR),
+      correctNetwork = MuxDataSdk.AndroidDevice.CONNECTION_TYPE_CELLULAR,
+      incorrectNetworks = listOf(
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_OTHER,
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIFI,
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIRED,
+      )
+    )
+    // WiFi Networks
+    testConnectivityCase(
+      connMgrReturns16 = listOf(ConnectivityManager.TYPE_WIFI),
+      connMgrReturns23 = listOf(NetworkCapabilities.TRANSPORT_WIFI),
+      correctNetwork = MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIFI,
+      incorrectNetworks = listOf(
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_OTHER,
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_CELLULAR,
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIRED,
+      )
+    )
+    // Wired Networks
+    testConnectivityCase(
+      connMgrReturns16 = listOf(ConnectivityManager.TYPE_ETHERNET),
+      connMgrReturns23 = listOf(NetworkCapabilities.TRANSPORT_ETHERNET),
+      correctNetwork = MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIRED,
+      incorrectNetworks = listOf(
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_OTHER,
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_CELLULAR,
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIFI,
+      )
+    )
+    // Misc Networks
+    testConnectivityCase(
+      connMgrReturns16 = listOf(
+        ConnectivityManager.TYPE_BLUETOOTH,
+        ConnectivityManager.TYPE_DUMMY,
+        ConnectivityManager.TYPE_VPN,
+      ),
+      connMgrReturns23 = listOf(
+        8 /*not in api23, NetworkCapabilities.TRANSPORT_USB,*/,
+        6 /*not in api23, NetworkCapabilities.TRANSPORT_LOWPAN*/,
+        5 /*not in api23, NetworkCapabilities.TRANSPORT_WIFI_AWARE*/,
+        NetworkCapabilities.TRANSPORT_VPN,
+      ),
+      correctNetwork = MuxDataSdk.AndroidDevice.CONNECTION_TYPE_OTHER,
+      incorrectNetworks = listOf(
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIRED,
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_CELLULAR,
+        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIFI,
+      )
+    )
+  }
+
+  private fun testConnectivityCase(
+    connMgrReturns16: Collection<Int>,
+    connMgrReturns23: Collection<Int>,
+    correctNetwork: String,
+    incorrectNetworks: List<String>
+  ) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      // Should be on Cellular
-      testConnectionType23(
-        "api 23: cellular network detected",
-        NetworkCapabilities.TRANSPORT_CELLULAR,
-        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_CELLULAR,
-        true
-      )
-      // Should not be on these
-      testConnectionType23(
-        "api 23: cellular network detected",
-        NetworkCapabilities.TRANSPORT_CELLULAR,
-        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIFI,
-        false
-      )
-      testConnectionType23(
-        "api 23: cellular network detected",
-        NetworkCapabilities.TRANSPORT_CELLULAR,
-        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIRED,
-        false
-      )
-      testConnectionType23(
-        "api 23: cellular network detected",
-        NetworkCapabilities.TRANSPORT_CELLULAR,
-        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_OTHER,
-        false
-      )
+      // Correct case
+      connMgrReturns23.onEach { transport ->
+        testConnectionType23(
+          assertMsg = "API 23: network type should be $correctNetwork",
+          mockTypeFromConnMgr = transport,
+          expectedType = correctNetwork,
+          onThisNetwork = true
+        )
+      }
+      // Incorrect cases
+      incorrectNetworks.onEach { incorrectNet ->
+        connMgrReturns23.onEach { connType ->
+          testConnectionType23(
+            assertMsg = "API 23: network type should not be $correctNetwork",
+            mockTypeFromConnMgr = connType,
+            expectedType = incorrectNet,
+            onThisNetwork = false
+          )
+        }
+      }
     } else {
-      // Should be on Cellular
-      testConnectionType16(
-        "api 16: cellular network detected",
-        ConnectivityManager.TYPE_MOBILE,
-        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_CELLULAR,
-        true
-      )
-      // Should not be on these
-      testConnectionType16(
-        "api 16: cellular network detected",
-        ConnectivityManager.TYPE_MOBILE,
-        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIFI,
-        false
-      )
-      testConnectionType16(
-        "api 16: cellular network detected",
-        ConnectivityManager.TYPE_MOBILE,
-        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_WIRED,
-        false
-      )
-      testConnectionType16(
-        "api 16: cellular network detected",
-        ConnectivityManager.TYPE_MOBILE,
-        MuxDataSdk.AndroidDevice.CONNECTION_TYPE_OTHER,
-        false
-      )
+      // Correct case
+      connMgrReturns16.onEach { transport ->
+        testConnectionType16(
+          assertMsg = "API 16: network type should be $correctNetwork",
+          mockTypeFromConnMgr = transport,
+          expectedType = correctNetwork,
+          onThisNetwork = true
+        )
+      }
+      // Incorrect cases
+      incorrectNetworks.onEach { incorrectNet ->
+        connMgrReturns16.onEach { connType ->
+          testConnectionType16(
+            assertMsg = "API 16: network type should not be $correctNetwork",
+            mockTypeFromConnMgr = connType,
+            expectedType = incorrectNet,
+            onThisNetwork = false
+          )
+        }
+      }
     }
   }
 
   @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
   private fun testConnectionType16(
     assertMsg: String,
-    typeFromConnMgr: Int,
+    mockTypeFromConnMgr: Int,
     expectedType: String,
     onThisNetwork: Boolean,
   ) {
-    val ctx = mockActivity(connMgr = mockConnectivityManager16(typeFromConnMgr))
+    val ctx = mockActivity(connMgr = mockConnectivityManager16(mockTypeFromConnMgr))
     if (onThisNetwork) {
       assertEquals(assertMsg, expectedType, device(ctx).networkConnectionType)
     } else {
@@ -92,11 +141,11 @@ class AndroidDeviceTests : AbsRobolectricTest() {
   @TargetApi(Build.VERSION_CODES.M)
   private fun testConnectionType23(
     assertMsg: String,
-    typeFromConnMgr: Int,
+    mockTypeFromConnMgr: Int,
     expectedType: String,
     onThisNetwork: Boolean,
   ) {
-    val ctx = mockActivity(connMgr = mockConnectivityManager23(typeFromConnMgr))
+    val ctx = mockActivity(connMgr = mockConnectivityManager23(mockTypeFromConnMgr))
     if (onThisNetwork) {
       assertEquals(assertMsg, expectedType, device(ctx).networkConnectionType)
     } else {
