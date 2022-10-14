1e3f14f1f744c4ea9e4bc804d7bbde27d6ac3aaf
Emily Dixon <edixon@mux.com>
2022-10-14 14:02:10 -0700

    A comment

--- core-android/src/main/java/com/mux/stats/sdk/muxstats/internal/Util.kt
+++ core-android/src/main/java/com/mux/stats/sdk/muxstats/internal/Util.kt
@@ -23,7 +23,7 @@ internal fun <Any> allEqual(these: List<Any>): Boolean {
  * Gets a Log Tag from the name of the calling class. Can be used in any package that isn't
  * obfuscated (such as muxstats)
  */
-@Suppress("unused") // Any is used for its class
+@Suppress("unused") // T is used for its class
 internal inline fun <reified T> T.logTag() = T::class.java.simpleName
 
 /**
