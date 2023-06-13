8dc73e91a5d5a75f55809763b9e98e8d7ac0b570
Emily Dixon <edixon@mux.com>
2023-06-13 15:10:29 -0700

    Little deprecation thing

--- core-android/src/main/java/com/mux/stats/sdk/muxstats/MuxDataSdk.kt
+++ core-android/src/main/java/com/mux/stats/sdk/muxstats/MuxDataSdk.kt
@@ -437,15 +437,19 @@ abstract class MuxDataSdk<Player, PlayerView : View> @JvmOverloads protected con
             MuxLogger.w(TAG, "Could not get network capabilities")
             null
           }
+
           nc.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
             CONNECTION_TYPE_WIRED
           }
+
           nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
             CONNECTION_TYPE_WIFI
           }
+
           nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
             CONNECTION_TYPE_CELLULAR
           }
+
           else -> {
             CONNECTION_TYPE_OTHER
           }
@@ -469,9 +473,11 @@ abstract class MuxDataSdk<Player, PlayerView : View> @JvmOverloads protected con
             ConnectivityManager.TYPE_ETHERNET -> {
               CONNECTION_TYPE_WIRED
             }
+
             ConnectivityManager.TYPE_WIFI -> {
               CONNECTION_TYPE_WIFI
             }
+
             ConnectivityManager.TYPE_MOBILE,
             ConnectivityManager.TYPE_MOBILE_DUN,
             ConnectivityManager.TYPE_MOBILE_HIPRI,
@@ -480,6 +486,7 @@ abstract class MuxDataSdk<Player, PlayerView : View> @JvmOverloads protected con
             ConnectivityManager.TYPE_MOBILE_MMS -> {
               CONNECTION_TYPE_CELLULAR
             }
+
             else -> {
               CONNECTION_TYPE_OTHER
             }
@@ -557,9 +564,13 @@ abstract class MuxDataSdk<Player, PlayerView : View> @JvmOverloads protected con
     init {
       deviceId = getOrCreateDeviceId(ctx)
       try {
-        val pi = ctx.packageManager.getPackageInfo(ctx.packageName, 0)
-        appName = pi.packageName
-        appVersion = pi.versionName
+        val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+          getPackageInfoApi33(ctx)
+        } else {
+          getPackageInfoLegacy(ctx)
+        }
+        appName = packageInfo.packageName
+        appVersion = packageInfo.versionName
       } catch (e: PackageManager.NameNotFoundException) {
         MuxLogger.d(TAG, "could not get package info")
       }
