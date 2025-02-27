de97888579a01f1faaf9f644a9f8b56acf0b6405
Emily Dixon <edixon@mux.com>
2025-02-27 09:41:20 -0800

    Add overloads for some utils that work on collections

--- library/src/main/java/com/mux/android/util/Util.kt
+++ library/src/main/java/com/mux/android/util/Util.kt
@@ -12,9 +12,36 @@ fun <Any> Any.oneOf(vararg these: Any) = these.contains(this)
 /**
  * Returns true if the receiver is not in the given objects.
  *
+ * For example `"blue".oneOf("red", "green") == false` and `3.oneOf(3,5,6) == true`
+ */
+fun <Any> Any.oneOf(these: List<Any>) = these.contains(this)
+
+/**
+ * Returns true if the receiver is not in the given objects.
+ *
  * For example `3.noneOf(1,4,5) == true` and `3.noneOf(3,5,6) = false`
  */
 fun <Any> Any.noneOf(vararg these: Any) = !these.contains(this)
+
+/**
+ * Returns true if the receiver is not in the given objects.
+ *
+ * For example `3.noneOf(1,4,5) == true` and `3.noneOf(3,5,6) = false`
+ */
+fun <Any> Any.noneOf(these: List<Any>) = !these.contains(this)
+
+/**
+ * Return true if all elements in the given set are equal
+ */
+fun <Any> allEqual(these: List<Any>): Boolean {
+  if (these.isEmpty()) {
+    return true
+  } else {
+    val head = these.first()
+    these.slice(1..these.lastIndex).onEach { if (head != it) return false }
+    return true
+  }
+}
 
 /**
  * Return true if all elements in the given set are equal
